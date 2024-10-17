ee89780f5eafed31f656cbe4d90586d9da3176df
Saksham1515 <sakshamcse2021@gmail.com>
2024-10-18 02:49:21 +0530

    l

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import streamlit as st
 import graphviz
 import json
-os.environ['GOOGLE_APU_KEY']="AIzaSyBJtToNkzQeaPV1-3NT_6iHsRNFgcYR7y8"
+os.environ['GOOGLE_APU_KEY']=os.getenv("api_key")
 genai.configure(api_key=os.environ['GOOGLE_APU_KEY'])
 model = genai.GenerativeModel('gemini-pro')
 def to_markdown(text):
